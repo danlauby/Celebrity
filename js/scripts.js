94d34e33d4ddaaef01622252790c9abe4eed45b8
danlauby <dmlauby@gmail.com>
2017-01-12 15:36:28 -0800

    fix conditional to hide 'noMatch' class when conditions are true

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,17 @@
-// $(function() {
-//   $('form#input').submit(function(e) {
-//     var age = parseInt($('input#age').val());
-//     var color = $('select#color').val();
-//     if(age) {
-//       var tom = "Tom Waits";
-//       var gordon = "Gordon Ramsey";
-//       if(age > 50 && color === 'green') {
-//         $('#celebrity').text(tom);
-//         $('#match').show();
-//       } else if(age < 50 && color === 'blue') {
-//         $('#celebrity').text(gordon);
-//         $('#noMatch').hide();
-//         $('#match').show();
-//       } else {
-//         $('#match').hide();
-//         $('#noMatch').show();
-//       }
-//     } else {
-//
-//     }
-//     e.preventDefault();
-//   });
-// });
-
 $(function() {
   $('form#input').submit(function(e) {
     var age = parseInt($('input#age').val());
     var color = $('select#color').val();
-
     if (age) {
       var tom = "Tom Waits";
       var johnny = "Johnny Greenwood";
-      if (age < 50 && color === 'blue') {
+      if (age < 50 && color === 'green') {
         $('#celebrity').text(johnny);
+        $('#noMatch').hide();
         $('#match').show();
-      } else if (age >= 50 && color === 'green') {
+      } else if (age >= 50 && color === 'blue') {
         $('#celebrity').text(tom);
+        $('#noMatch').hide();
         $('#match').show();
       } else {
         $('#match').hide();
